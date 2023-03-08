abe67ee802b87f0dfc69322f9f84443e2efd6ed7
Jaimin Suthar <85049765+Jaimin78@users.noreply.github.com>
2023-03-08 05:05:27 +0000

    Footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Navigation from './Navigation';
+import Footer from './Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,9 +12,11 @@ function App() {
           <Route path="/" element= {<h1>Home</h1>} />
           <Route path="/profile" element= {<h1>Profile</h1>} />
           <Route path="/add" element= {<h1>Product add</h1>} />
-          <Route path="/update" element= {<h1>Product update</h1>} />
+          <Route path="/update" element= {<h1>Product update</h1>} />       
+          <Route path="/logout" element= {<h1>Logout</h1>} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <>
+      <div className="footer">
+        Made with &hearts; By Jaimin Suthar
+      </div>
+    </>
+  )
+}
+
+export default Footer;
