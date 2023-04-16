69046edf7f1b5e22bf384ab7f071cb29e715728d
Jaimin Suthar <85049765+Jaimin78@users.noreply.github.com>
2023-04-16 10:48:51 +0000

    Integrated Product Search API

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   const [productList, setProductList] = useState([]);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     getProduct();
-  },[])
+  }, [])
 
   const getProduct = async () => {
     let data = await fetch('https://e-commerce-dashboard-backend.jaiminsuthar.repl.co/api/product');
@@ -16,48 +16,67 @@ const Home = () => {
   }
 
   const productDelete = async (id) => {
-     let remove = await fetch(`https://e-commerce-dashboard-backend.jaiminsuthar.repl.co/api/product/delete/${id}`,{
-       method:'delete'
-     })
-     if(remove){
-       getProduct()
-     }
+    let remove = await fetch(`https://e-commerce-dashboard-backend.jaiminsuthar.repl.co/api/product/delete/${id}`, {
+      method: 'delete'
+    })
+    if (remove) {
+      getProduct()
+    }
   }
-  
-  return(
+
+  const searchProduct = async (event) => {
+    let key = event.target.value;
+    if (key) {
+      let search = await fetch(`https://e-commerce-dashboard-backend.jaiminsuthar.repl.co/api/product/search/${key}`);
+      search = await search.json();
+      setProductList(search)
+    } else {
+      getProduct()
+    }
+  }
+
+  return (
     <>
       <main>
-      <h1>Product List</h1>
-      <Table className="container" striped bordered hover responsive variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Image</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Brand</th>
-          <th>Category</th>
-          <th colSpan={2}>Operation</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          productList.map((item,i)=>
-         <tr key={i}>
-          <td>{i+1}</td>
-          <td><img style={{width:50}} src={item.imageUrl}/></td>
-          <td>{item.name}</td>
-          <td>₹{item.price}</td>
-          <td>{item.brand}</td>
-          <td>{item.category}</td>
-          <td><Button className="btn btn-success"><Link to={`/update/${item._id}`}>Update</Link></Button></td>
-          <td><Button className="btn btn-danger" onClick={() => productDelete(item._id)}>Delete</Button></td>
-        </tr>)
-        }
-      </tbody>
-    </Table>
-    </main>
-   </>
+        <div className="container my-4">
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="inputGroup-sizing-default">Search</span>
+            </div>
+            <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" onChange={searchProduct} />
+          </div>
+        </div>
+        <h1>Product List</h1>
+        <Table className="container" striped bordered hover responsive variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Brand</th>
+              <th>Category</th>
+              <th colSpan={2}>Operation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              productList.map((item, i) =>
+                <tr key={i}>
+                  <td>{i + 1}</td>
+                  <td><img style={{ width: 50 }} src={item.imageUrl} /></td>
+                  <td>{item.name}</td>
+                  <td>₹{item.price}</td>
+                  <td>{item.brand}</td>
+                  <td>{item.category}</td>
+                  <td><Button className="btn btn-success"><Link to={`/update/${item._id}`}>Update</Link></Button></td>
+                  <td><Button className="btn btn-danger" onClick={() => productDelete(item._id)}>Delete</Button></td>
+                </tr>)
+            }
+          </tbody>
+        </Table>
+      </main>
+    </>
   )
 }
 
